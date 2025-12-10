26510a0e5d90524f6e1a5b6c66b628bc5f50d098
Mohamed Zoraa <mohamedzoraa100@gmail.com>
2025-12-10 10:18:26 +0100

    fix map error3

--- components/Featured.tsx
+++ components/Featured.tsx
@@ -1,40 +1,57 @@
-import { events } from '@/data/events'
+// import { events } from '@/data/events'
 import React from 'react'
 import EventCard from './EventCard'
 
 async function Featured() {
-  const fetchEvents=async()=>{
-    try {
-      const res=await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/event`)
-      const data=await res.json()
-      if(data.success){
-        return data.data
-      }
-    } catch (error) {
-      return {events:[],totalCount:0,error:"Failed to fetch events"}
-    }
-  }
-  const {events=[],error=""}:any =await fetchEvents()
+  // const fetchEvents = async () => {
+  //   try {
+  //     const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/event`, {
+  //       // ensure this works during prerender
+  //       cache: "no-store",
+  //     });
+  //     const data = await res.json();
 
-  if(error){
-    return <div className='bg-gray-100 py-32 ' id='featured'>
-      <h1 className="text-3xl font-bold mb-8 text-center text-gray-700">Featured Events</h1>
-    </div>
-  }
-  if(events) return (
-    <div className='bg-gray-100 py-32 ' id='featured'>
-      <h1 className="text-3xl font-bold mb-8 text-center text-gray-700">Featured Events</h1>
-        <div className='container mx-auto px-4 flex flex-wrap gap-4 justify-start'>
-            {events.map((e:any)=>(
-                <EventCard key={e._id} booked={20} {...e} />
-            ))}
-        </div>
-    </div>
+  //     if (data.success && Array.isArray(data.data)) {
+  //       return { events: data.data, error: "" };
+  //     }
 
-  )
-  if(!events) return <div className='bg-gray-100 py-32 ' id='featured'>
-    <h1 className="text-3xl font-bold mb-8 text-center text-gray-700">Featured Events</h1>
-  </div>
+  //     return {
+  //       events: [],
+  //       error: "Failed to fetch events",
+  //     };
+  //   } catch (error) {
+  //     return {
+  //       events: [],
+  //       error: "Failed to fetch events",
+  //     };
+  //   }
+  // };
+
+  // const { events, error } = await fetchEvents();
+
+  // if (error) {
+  //   return (
+  //     <div className="bg-gray-100 py-32" id="featured">
+  //       <h1 className="text-3xl font-bold mb-8 text-center text-gray-700">
+  //         Featured Events
+  //       </h1>
+  //       {/* Optionally show the error text */}
+  //     </div>
+  //   );
+  // }
+
+  return (
+    <div className="bg-gray-100 py-32" id="featured">
+      <h1 className="text-3xl font-bold mb-8 text-center text-gray-700">
+        Featured Events
+      </h1>
+      <div className="container mx-auto px-4 flex flex-wrap gap-4 justify-start">
+        {/* {events.map((e: any) => (
+          <EventCard key={e._id} booked={20} {...e} />
+        ))} */}
+      </div>
+    </div>
+  );
 }
 
 export default Featured
